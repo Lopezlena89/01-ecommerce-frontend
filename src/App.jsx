cdf8315558c479339596b83e35125f1a419b4408
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2023-09-04 19:59:27 -0600

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { LoginPage } from './auth/components/LoginPage'
 import { RegisterPage } from './auth/components/registerPage'
 
 export const App = () => {
   return (
     <>
-        <RegisterPage/>
+        {/* <RegisterPage/> */}
+        <LoginPage/>
     </>
   )
 }
--- src/auth/components/LoginPage.jsx
+++ src/auth/components/LoginPage.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export const LoginPage = () => {
   return (
-    <div>Login Page</div>
+    <>
+      <section className='registro-usuarios'>
+        <div className='container login'>
+          <div className='titulo col-3'>
+           <div> Login</div>
+          </div>
+          <div className='registro col-9'>
+            <div className="row-4">
+              <input type="text" placeholder='Correo'/>
+            </div>
+            <div className="row-4">
+              <input type="text" placeholder='Password'/>
+            </div>
+            <div className="row-4">
+              <a href="">Registro</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
